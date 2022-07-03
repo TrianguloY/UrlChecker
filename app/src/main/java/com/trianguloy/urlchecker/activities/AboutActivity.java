f45ffc723c7a2e114d16cf0fc9a907721ade46f8
TrianguloY <correo--correo@hotmail.com>
2022-07-03 20:43:15 +0200

    Added F-droid link to about section
    (hopefully Play Store won't complain)
    
    closes #8

--- app/src/main/java/com/trianguloy/urlchecker/activities/AboutActivity.java
+++ app/src/main/java/com/trianguloy/urlchecker/activities/AboutActivity.java
@@ -12,6 +12,14 @@ import com.trianguloy.urlchecker.utilities.PackageUtilities;
 
 public class AboutActivity extends Activity {
 
+    // ------------------- constants -------------------
+
+    public static final String BLOG = "https://triangularapps.blogspot.com/";
+    public static final String PLAY_STORE_PREFIX = "https://play.google.com/store/apps/details?id=";
+    public static final String F_DROID_PREFIX = "https://f-droid.org/packages/";
+
+    // ------------------- listeners -------------------
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,28 +27,57 @@ public class AboutActivity extends Activity {
         AndroidUtils.setActionBarColor(this);
     }
 
+    /**
+     * Button clicked
+     */
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.btn_blog:
+            case R.id.btn_openBlog:
                 // Open the blog
-                PackageUtilities.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://triangularapps.blogspot.com/")), R.string.toast_noBrowser, this);
+                open(BLOG);
                 break;
-            case R.id.btn_openStore:
+            case R.id.btn_openPlay:
                 // Open the Play Store
-                PackageUtilities.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + getPackageName())), R.string.toast_noBrowser, this);
+                open(PLAY_STORE_PREFIX + getPackageName());
                 break;
-            case R.id.btn_shareStore:
-                // Share the play store
-                Intent share = new Intent(android.content.Intent.ACTION_SEND);
-                share.setType("text/plain");
-                share.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            case R.id.btn_openDroid:
+                // Open F-Droid
+                open(F_DROID_PREFIX + getPackageName());
+                break;
 
-                // Add data to the intent, the receiving app will decide what to do with it.
-                share.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.app_name));
-                share.putExtra(Intent.EXTRA_TEXT, "https://play.google.com/store/apps/details?id=" + getPackageName());
-
-                PackageUtilities.startActivity(Intent.createChooser(share, getString(R.string.btn_shareStore)), R.string.toast_error, this);
+            case R.id.btn_sharePlay:
+                // Share play store
+                share(PLAY_STORE_PREFIX + getPackageName());
+                break;
+            case R.id.btn_shareDroid:
+                // Share F-droid
+                share(F_DROID_PREFIX + getPackageName());
                 break;
         }
+    }
+
+    // ------------------- actions -------------------
+
+    /**
+     * Open an url in the browser
+     */
+    private void open(String url) {
+        PackageUtilities.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)), R.string.toast_noBrowser, this);
+    }
+
+    /**
+     * Share an url as plain text
+     * (App name as subject)
+     */
+    private void share(String url) {
+        Intent share = new Intent(android.content.Intent.ACTION_SEND);
+        share.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+
+        // Add data to the intent, the receiving app will decide what to do with it.
+        share.setType("text/plain");
+        share.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.app_name));
+        share.putExtra(Intent.EXTRA_TEXT, url);
+
+        PackageUtilities.startActivity(Intent.createChooser(share, getString(R.string.btn_shareStore)), R.string.toast_error, this);
     }
 }
