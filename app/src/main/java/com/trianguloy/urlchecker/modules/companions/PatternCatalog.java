549410b5dcfeed46bcfce37ba35a6a4313f51dab
TrianguloY <correo--correo@hotmail.com>
2022-11-14 20:37:46 +0100

    extracted json catalog class

--- app/src/main/java/com/trianguloy/urlchecker/modules/companions/PatternCatalog.java
+++ app/src/main/java/com/trianguloy/urlchecker/modules/companions/PatternCatalog.java
@@ -1,10 +1,10 @@
 package com.trianguloy.urlchecker.modules.companions;
 
 import android.app.Activity;
+import android.content.Context;
 
 import com.trianguloy.urlchecker.R;
-import com.trianguloy.urlchecker.dialogs.JsonEditor;
-import com.trianguloy.urlchecker.utilities.InternalFile;
+import com.trianguloy.urlchecker.utilities.JsonCatalog;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -13,102 +13,53 @@ import org.json.JSONObject;
 /**
  * Represents the catalog of the Pattern module
  */
-public class PatternCatalog {
-
-    private final InternalFile custom = new InternalFile("patterns");
-
-    private final Activity cntx;
+public class PatternCatalog extends JsonCatalog {
 
     public PatternCatalog(Activity cntx) {
-        this.cntx = cntx;
-        custom.init(cntx);
+        super(cntx, "patterns", R.string.mPttrn_editor);
     }
 
-    /**
-     * Returns the current catalog
-     */
-    public JSONObject getCatalog() {
-        // get the updated file first
-        try {
-            String content = custom.get();
-            if (content != null) return new JSONObject(content);
-        } catch (JSONException ignored) {
-        }
+    @Override
+    public JSONObject buildBuiltIn(Context cntx) throws JSONException {
+        return new JSONObject()
 
-        // no updated file or can't read, use built-in one
-        return getBuiltIn();
-    }
+                // built from the translated strings
+                .put(cntx.getString(R.string.mPttrn_ascii), new JSONObject()
+                        .put("regex", "[^\\p{ASCII}]")
+                )
+                .put(cntx.getString(R.string.mPttrn_http), new JSONObject()
+                        .put("regex", "^http://")
+                        .put("replacement", "https://")
+                )
+                .put(cntx.getString(R.string.mPttrn_noSchemeHttp), new JSONObject()
+                        .put("regex", "^(?!.*:)")
+                        .put("replacement", "http://$0")
+                )
+                .put(cntx.getString(R.string.mPttrn_noSchemeHttps), new JSONObject()
+                        .put("regex", "^(?!.*:)")
+                        .put("replacement", "https://$0")
+                )
 
-    /**
-     * Gets the builtin patterns
-     */
-    public JSONObject getBuiltIn() {
-        try {
-            return new JSONObject()
-
-                    // built from the translated strings
-                    .put(cntx.getString(R.string.mPttrn_ascii), new JSONObject()
-                            .put("regex", "[^\\p{ASCII}]")
-                    )
-                    .put(cntx.getString(R.string.mPttrn_http), new JSONObject()
-                            .put("regex", "^http://")
-                            .put("replacement", "https://")
-                    )
-                    .put(cntx.getString(R.string.mPttrn_noSchemeHttp), new JSONObject()
-                            .put("regex", "^(?!.*:)")
-                            .put("replacement", "http://$0")
-                    )
-                    .put(cntx.getString(R.string.mPttrn_noSchemeHttps), new JSONObject()
-                            .put("regex", "^(?!.*:)")
-                            .put("replacement", "https://$0")
-                    )
-
-                    // privacy redirections samples (see https://github.com/TrianguloY/UrlChecker/discussions/122)
-                    .put("Reddit ➔ Teddit", new JSONObject()
-                            .put("regex", "^https?://(?:[a-z0-9-]+\\.)*?reddit.com/(.*)")
-                            .put("replacement", "https://teddit.net/$1")
-                            .put("enabled", "false")
-                    )
-                    .put("Twitter ➔ Nitter", new JSONObject()
-                            .put("regex", "^https?://(?:[a-z0-9-]+\\.)*?twitter.com/(.*)")
-                            .put("replacement", "https://nitter.net/$1")
-                            .put("enabled", "false")
-                    )
-                    .put("Youtube ➔ Invidious", new JSONObject()
-                            .put("regex", "^https?://(?:[a-z0-9-]+\\.)*?youtube.com/(.*)")
-                            .put("replacement", new JSONArray()
-                                    .put("https://yewtu.be/$1")
-                                    .put("https://farside.link/invidious/$1")
-                            )
-                            .put("enabled", "false")
-                    )
-                    ;
-        } catch (JSONException e) {
-            e.printStackTrace();
-            return new JSONObject();
-        }
-    }
-
-    /**
-     * Saves a json as new catalog
-     */
-    public boolean save(JSONObject content) {
-
-        // same as builtin (maybe a reset?), delete custom
-        if (content.equals(getBuiltIn())) {
-            custom.delete();
-            return true;
-        }
-
-        // store
-        return custom.set(content.toString());
-    }
-
-    /**
-     * Shows a dialog to manually edit the catalog
-     */
-    public void showEditor() {
-        JsonEditor.show(getCatalog(), getBuiltIn(), R.string.mPttrn_editor, cntx, this::save);
+                // privacy redirections samples (see https://github.com/TrianguloY/UrlChecker/discussions/122)
+                .put("Reddit ➔ Teddit", new JSONObject()
+                        .put("regex", "^https?://(?:[a-z0-9-]+\\.)*?reddit.com/(.*)")
+                        .put("replacement", "https://teddit.net/$1")
+                        .put("enabled", "false")
+                )
+                .put("Twitter ➔ Nitter", new JSONObject()
+                        .put("regex", "^https?://(?:[a-z0-9-]+\\.)*?twitter.com/(.*)")
+                        .put("replacement", "https://nitter.net/$1")
+                        .put("enabled", "false")
+                )
+                .put("Youtube ➔ Invidious", new JSONObject()
+                        .put("regex", "^https?://(?:[a-z0-9-]+\\.)*?youtube.com/(.*)")
+                        .put("replacement", new JSONArray()
+                                .put("https://yewtu.be/$1")
+                                .put("https://farside.link/invidious/$1")
+                        )
+                        .put("enabled", "false")
+                )
+                ;
     }
 
 }
---
+++ app/src/main/java/com/trianguloy/urlchecker/utilities/JsonCatalog.java
@@ -0,0 +1,80 @@
+package com.trianguloy.urlchecker.utilities;
+
+import android.app.Activity;
+import android.content.Context;
+
+import com.trianguloy.urlchecker.dialogs.JsonEditor;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Represents a generic catalog
+ */
+public abstract class JsonCatalog {
+
+    private final Activity cntx;
+    private final InternalFile custom;
+    private final int editorDescription;
+
+    public JsonCatalog(Activity cntx, String fileName, int editorDescription) {
+        this.cntx = cntx;
+        this.editorDescription = editorDescription;
+        custom = new InternalFile(fileName).init(cntx);
+    }
+
+    /**
+     * Returns the current catalog
+     */
+    public JSONObject getCatalog() {
+        // get the updated file first
+        try {
+            String content = custom.get();
+            if (content != null) return new JSONObject(content);
+        } catch (JSONException ignored) {
+        }
+
+        // no updated file or can't read, use built-in one
+        return getBuiltIn();
+    }
+
+    /**
+     * Gets the builtin catalog
+     */
+    public JSONObject getBuiltIn() {
+        try {
+            return buildBuiltIn(cntx);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return new JSONObject();
+        }
+    }
+
+    /**
+     * Builds the builtIn cattalog
+     */
+    abstract public JSONObject buildBuiltIn(Context cntx) throws JSONException;
+
+    /**
+     * Saves a json as new catalog
+     */
+    public boolean save(JSONObject content) {
+
+        // same as builtin (maybe a reset?), delete custom
+        if (content.equals(getBuiltIn())) {
+            custom.delete();
+            return true;
+        }
+
+        // store
+        return custom.set(content.toString());
+    }
+
+    /**
+     * Shows a dialog to manually edit the catalog
+     */
+    public void showEditor() {
+        JsonEditor.show(getCatalog(), getBuiltIn(), editorDescription, cntx, this::save);
+    }
+
+}
