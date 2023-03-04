2b3f1de9126e113e77ba1f9eb094de8f0bdab14e
Pablo Ortigosa <55493443+PabloOQ@users.noreply.github.com>
2023-03-04 12:04:55 +0000

    Fix UnaryOperator

--- app/src/main/java/com/trianguloy/urlchecker/utilities/GenericPref.java
+++ app/src/main/java/com/trianguloy/urlchecker/utilities/GenericPref.java
@@ -15,7 +15,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import java.util.function.UnaryOperator;
 
 /**
  * A generic type preference
---
+++ app/src/main/java/com/trianguloy/urlchecker/utilities/UnaryOperator.java
@@ -0,0 +1,5 @@
+package com.trianguloy.urlchecker.utilities;
+
+public interface UnaryOperator <T>{
+    T apply(T o);
+}
