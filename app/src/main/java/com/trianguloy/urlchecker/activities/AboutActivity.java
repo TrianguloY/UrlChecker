2eeff096945ac88968130097b133bb7202df9e80
TrianguloY <correo--correo@hotmail.com>
2023-04-14 18:27:26 +0200

    build evaluation apk on prs
    Idea from Ilithy

--- app/src/main/java/com/trianguloy/urlchecker/activities/AboutActivity.java
+++ app/src/main/java/com/trianguloy/urlchecker/activities/AboutActivity.java
@@ -42,9 +42,12 @@ public class AboutActivity extends Activity {
         setTitle(R.string.a_about);
         AndroidUtils.configureUp(this);
 
-        // on release, append version to the action bar title
         if (!BuildConfig.DEBUG) {
+            // on release, append version to the action bar title
             setTitle(getTitle() + " (V" + BuildConfig.VERSION_NAME + ")");
+        } else if (!"alpha".equals(BuildConfig.BUILD_TYPE)) {
+            // on no-alpha, append type
+            setTitle(getTitle() + " (" + BuildConfig.BUILD_TYPE + ")");
         }
 
         // fill contributors and translators
